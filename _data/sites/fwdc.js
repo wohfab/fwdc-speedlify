8dd16872ceb25fff5ad2a5b9360a7567dbabb24a
fabian wohlgemuth <wohfab@gmail.com>
2021-02-24 20:04:04 +0100

    Update fwdc.js

--- _data/sites/fwdc.js
+++ _data/sites/fwdc.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "fwdc projects, templates, customer sites",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: false,
 	options: {
 		frequency: 60 * 23, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
